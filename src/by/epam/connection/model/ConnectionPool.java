df64310b0b04de42cc9eada2a59ad690470c1e96
Max.Burishinets <arhor18@gmail.com>
2018-08-23 10:11:15 +0300

    properties handler added

--- src/by/epam/connection/model/ConnectionPool.java
+++ src/by/epam/connection/model/ConnectionPool.java
@@ -1,34 +1,48 @@
 package by.epam.connection.model;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import by.epam.connection.service.PropertiesHandler;
+
 public class ConnectionPool {
 	private static final Logger LOG = LogManager.getLogger(ConnectionPool.class);
 	
+	public static final String DB_URL = "db.url";
+	public static final String DB_USER = "db.user";
+	public static final String DB_PASSWORD = "db.password";
+	public static final String DB_POOLSIZE = "db.poolsize";
+	
 	public void initialize() {
+		Properties prop = null;
+		try {
+			prop = PropertiesHandler.readProperties("resources/connectionpool.properties");
+			LOG.info("properties successfully loaded\n");
+		} catch (IOException e) {
+			LOG.debug("loading properties error: ", e);
+		}
 		Connection connection = null;
-		String url = "jdbc:mysql://localhost:3306/admission_committee"+
-                "?verifyServerCertificate=false"+
-                "&useSSL=false"+
-                "&requireSSL=false"+
-                "&useLegacyDatetimeCode=false"+
-                "&amp"+
-                "&serverTimezone=UTC";
+//		String url = "jdbc:mysql://localhost:3306/admission_committee"+
+//                "?verifyServerCertificate=false"+
+//                "&useSSL=false"+
+//                "&requireSSL=false"+
+//                "&useLegacyDatetimeCode=false"+
+//                "&amp"+
+//                "&serverTimezone=UTC";
 		try {
 			java.sql.DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver()); // find real driver name
 			// TODO: implement connection wrapper !!!
 			connection = java.sql.DriverManager.getConnection(
-					url,
-					"root",
-					"dragonlance");
+					prop.getProperty(DB_URL),
+					prop.getProperty(DB_USER),
+					prop.getProperty(DB_PASSWORD));
 			LOG.info("Connection established\n");
 			Statement st = connection.createStatement();
 			ResultSet rs = st.executeQuery("SELECT * FROM `faculties`");
